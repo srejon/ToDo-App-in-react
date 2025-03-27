9ff1ba5f6ef24ab52944cdb62583f9b642e5c61e
Srejon Sharma <srejonboss2@gmail.com>
2025-03-27 23:18:31 +0600

    js updated

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -8,15 +8,34 @@ const ToDoApp = () => {
             setItems([...items, inputValue]);
             setInputValue('')
         }
-    }
+    };
+
+    const removeItems = (index) => {
+        const updatedItems = items.filter((_, i) => i !== index);
+        setItems(updatedItems);
+    };
+    const handleKeyPress = (e) => {
+        if(e.key === "Enter"){
+            addItems();
+        }
+    };
     return(
-        <>
         <div className="container">
         <h1>TODO APP</h1>
-        <input type="text" placeholder="Enter your task" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        <div className="todo-input">
+        <input type="text" placeholder="Enter your task" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+        onKeyPress={handleKeyPress}/>
         <button onClick={addItems}>Add</button>
         </div>
-        </>
+        <ul className="todo-list">
+            {items.map((item, index) => (
+                <li key={index} className="todo-item">
+                    <span>{item}</span>
+                    <button onClick={()=> removeItems(index)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+        </div>
     )
 }
 
